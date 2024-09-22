3e2a79f18d892d2e5f4269d9a936620cd6e1d215
Thomah <5090230+Thomah@users.noreply.github.com>
2024-09-22 20:01:42 +0200

    Add beginning of script

---
+++ generate_script_pdf.rb
@@ -0,0 +1,16 @@
+require 'asciidoctor'
+require 'asciidoctor-pdf'
+
+input_dir = 'script'
+output_file = 'output.pdf'
+
+adoc_files = Dir.glob("#{input_dir}/*.adoc")
+
+combined_content = ""
+adoc_files.each do |file|
+  combined_content += File.read(file) + "\n"
+end
+
+Asciidoctor.convert(combined_content, backend: 'pdf', safe: :safe, to_file: output_file, attributes: { 'stylesdir' => 'script/', 'stylesheet' => 'styles.css' })
+
+puts "Generated PDF : #{output_file}"
